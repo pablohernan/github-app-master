5c8a30dd97f0fb5135faa52c79ff9282f05e2e75
User <user@gmail.com>
2018-01-16 14:24:27 -0200

    autoupdate `date +T`

--- public/js/main.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Attachments from './attachments.jsx';
-
-ReactDOM.render(<Attachments />, document.getElementById('attachments'));
