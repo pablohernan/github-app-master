08e265ba71ca77529a3c16758ed6f8d21b84d212
User <user@gmail.com>
2018-01-21 01:57:55 -0200

    Signed-off-by: User <user@gmail.com>

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,5 @@
-/* globals PipefyApp */
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import Attachments from './attachments.jsx';
 
-const pipefy = PipefyApp.init();
-
-class Attachments extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = { pipe: null };
-  }
-  componentDidMount() {
-    PipefyApp.render(() => {
-      pipefy.pipe().then((pipe) => {
-        this.setState({ pipe })
-      });
-    });
-  }
-  
-  render() {
-    if (!this.state.pipe) { return <div /> };
-    return <h1>Hello from React on Pipe: {this.state.pipe.name}</h1>
-  }
-}
-
-
-render(<Attachments />, document.getElementById('attachments'));
+ReactDOM.render(<Attachments />, document.getElementById('attachments'));
